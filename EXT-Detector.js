3320ae55b259e8d35b1c17608da4b6f038ef1468
bugsounet <bugsounet@bugsounet.fr>
2023-02-20 23:05:49 +0100

    simplify main core

--- EXT-Detector.js
+++ EXT-Detector.js
@@ -1,5 +1,5 @@
 //
-// Module : EXT-Detector
+// Module : EXT-Detector v2
 // Snowboy and Porcupine keywords listener for GAv4
 // @bugsounet
 //
@@ -36,19 +36,17 @@ Module.register("EXT-Detector", {
 
   start: function() {
     this.ready = false
-    this.listening = false
-    this.logoGoogle= this.file("resources/google.png")
-    this.micConfig= {
+    this.config.mic= {
       recorder: "auto",
       device: "default"
     }
-    this.config.mic= this.micConfig
     this.config.snowboyMicConfig= configMerge({}, this.config.mic, this.config.snowboyMicConfig)
     if (this.config.touchOnly) this.config.useIcon = true
+    this.visual = new DetectorVisual(this)
     this.sendSocketNotification("INIT", this.config)
   },
 
-  notificationReceived: function(notification, payload) {
+  notificationReceived: function(notification, payload, sender) {
     switch (notification) {
       case "EXT_DETECTOR-START":
         if (this.ready) this.sendSocketNotification("START")
@@ -57,8 +55,10 @@ Module.register("EXT-Detector", {
         if (this.ready) this.sendSocketNotification("STOP")
         break
       case "GAv4_READY": // auto activate with GAv4
-        this.sendNotification("EXT_HELLO", this.name)
-        this.ready = true
+        if (sender.name == "MMM-GoogleAssistant") {
+          this.sendNotification("EXT_HELLO", this.name)
+          this.ready = true
+        }
         break
     }
   },
@@ -66,13 +66,13 @@ Module.register("EXT-Detector", {
   socketNotificationReceived: function(notification, payload) {
     switch (notification) {
       case "LISTENING":
-        this.refreshLogo(false)
+        this.visual.DetectorRefreshLogo(this,false)
         break
       case "DETECTED":
-        this.activateWord()
+        this.visual.DetectorActivateWord(this)
         break
       case "DISABLED":
-        this.disabled()
+        this.visual.DetectorDisabled(this)
         break
       case "ERROR":
         this.sendNotification("EXT_ALERT", {
@@ -100,68 +100,19 @@ Module.register("EXT-Detector", {
     }
   },
 
-  getDom: function() {
-    var wrapper = document.createElement('div')
-    wrapper.id = "EXT_DETECTOR"
-
-    if (this.config.useIcon) {
-      var icon = document.createElement('div')
-      icon.id= "EXT_DETECTOR-ICON"
-      icon.style.backgroundImage = "url("+this.logoGoogle+")"
-      icon.classList.add("busy")
-      icon.onclick = (event)=> {
-        event.stopPropagation()
-        this.clickCheck()
-      }
-      wrapper.appendChild(icon)
-    }
-    else wrapper.className = "hidden"
-    return wrapper
-  },
-
   getStyles: function(){
     return [
       this.file("EXT-Detector.css")
     ]
   },
 
-  refreshLogo: function(disabled) {
-    if (!this.config.useIcon) return
-    var icon = document.getElementById("EXT_DETECTOR-ICON")
-    if (disabled) {
-      this.listening = false
-      icon.classList.remove("busy")
-      icon.classList.add("flash")
-    } else {
-      this.listening = true
-      icon.classList.remove("busy")
-      icon.classList.remove("flash")
-    }
+  getScripts: function() {
+    return [
+      "/modules/EXT-Detector/components/visual.js"
+    ]
   },
 
-  disabled: function() {
-    if (!this.config.useIcon) return
-    this.listening = false
-    var icon = document.getElementById("EXT_DETECTOR-ICON")
-    icon.classList.add("busy")
-    icon.classList.remove("flash")
-  },
-
-  clickCheck: function() {
-    if (!this.listening) return
-    console.log("[DETECTOR] ~Activate by Touch~")
-    this.clickActivate()
-  },
-
-  clickActivate: function () {
-    this.sendSocketNotification("STOP", false) // stop and don't send DISABLED callback
-    this.listening = false
-    this.activateWord()
-  },
-
-  activateWord: function () {
-    this.refreshLogo(true)
-    console.log("[DETECTOR] ~Activate GAv4~")
-    this.sendNotification("GAv4_ACTIVATE")
+  getDom: function() {
+    return this.visual.DetectorDom(this)
   }
 })
