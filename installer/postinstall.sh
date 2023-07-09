b716aff4691ef31ad5cc8996874a76655dc8560a
bugsounet <github@bugsounet.fr>
2023-07-10 00:48:46 +0200

    try esbuild minify

--- installer/postinstall.sh
+++ installer/postinstall.sh
@@ -20,24 +20,25 @@ Installer_dir="$(Installer_get_current_dir)"
 cd "$Installer_dir"
 
 source utils.sh
-
-Installer_info "Minify Main code..."
-node minify.js
-Installer_success "Done"
-echo
-
-cd ..
-# module name
 Installer_module="EXT-Detector"
 
 Installer_info "Prepare snowboy library..."
-tsc -p tsconfig.json || {
+tsc -p ../tsconfig.json || {
   Installer_error "Failed!"
   exit 255
 }
 Installer_success "Done"
 echo
 
+Installer_info "Minify Main code..."
+node minify.js || {
+  Installer_error "Minify Failed!"
+  exit 255
+}
+Installer_success "Done"
+echo
+
+cd ..
 Installer_info "Rebuild MagicMirror..."
 MagicMirror-rebuild 2>/dev/null || {
   Installer_error "Rebuild Failed"
