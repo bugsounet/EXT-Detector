14e79ffd1bd89dd1885bb514f08ac9c33c2d993d
bugsounet <github@bugsounet.fr>
2024-02-18 13:50:36 +0100

    eslint fix (node_helper)

--- node_helper.js
+++ node_helper.js
@@ -1,202 +1,204 @@
-/*!********************
-* EXT-Detector helper
-* @bugsounet
-* 2024-02-08
-***********************/
+/*
+ * EXT-Detector helper
+ * @bugsounet
+ * 2024-02-08
+ */
 
-"use strict"
-var log = (...args) => { /* do nothing */ }
-var NodeHelper = require("node_helper")
+"use strict";
+let log = (...args) => { /* do nothing */ };
+const NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
-  start: function () {
-    this.config = {}
-    this.porcupine = null
-    this.porcupineConfig = []
-    this.snowboyConfig = {}
-    this.snowboy = null
-    this.Snowboy = []
-    this.Porcupine = []
-    this.porcupineCanRestart = false
-    this.detector = false
-    this.running = false
-    this.lib = {}
-    this.detectorModel = 0
+  start () {
+    this.config = {};
+    this.porcupine = null;
+    this.porcupineConfig = [];
+    this.snowboyConfig = {};
+    this.snowboy = null;
+    this.Snowboy = [];
+    this.Porcupine = [];
+    this.porcupineCanRestart = false;
+    this.detector = false;
+    this.running = false;
+    this.lib = {};
+    this.detectorModel = 0;
   },
 
-  socketNotificationReceived: function (notification, payload) {
-    switch(notification) {
+  socketNotificationReceived (notification, payload) {
+    switch (notification) {
       case "INIT":
-        this.config = payload
-        this.initialize()
-        break
+        this.config = payload;
+        this.initialize();
+        break;
       case "START":
-        if (!this.running) this.activate()
-        break
+        if (!this.running) { this.activate(); }
+        break;
       case "STOP":
-        if (this.running) this.deactivate(payload)
-        break
+        if (this.running) { this.deactivate(payload); }
+        break;
     }
   },
 
   /** [DATA] **/
-  initialize: async function() {
-    console.log("[DETECTOR] EXT-Detector Version:", require('./package.json').version , "rev:", require('./package.json').rev)
-    if (this.config.debug) log = (...args) => { console.log("[DETECTOR]", ...args) }
-    await this.detectorFilter()
-    let bugsounet = await this.libraries()
+  async initialize () {
+    console.log("[DETECTOR] EXT-Detector Version:", require("./package.json").version, "rev:", require("./package.json").rev);
+    if (this.config.debug) {
+      log = (...args) => { console.log("[DETECTOR]", ...args); };
+    }
+    await this.detectorFilter();
+    const bugsounet = await this.libraries();
     if (bugsounet) {
-      console.error(`[DETECTOR] [DATA] Warning: ${bugsounet} needed library not loaded !`)
-      console.error("[DETECTOR] [DATA] Try to solve it with: npm run rebuild")
-      return
+      console.error(`[DETECTOR] [DATA] Warning: ${bugsounet} needed library not loaded !`);
+      console.error("[DETECTOR] [DATA] Try to solve it with: npm run rebuild");
+      return;
     }
     if (this.config.touchOnly) {
-      console.log("[DETECTOR] [DATA] Ready with Touch Screen Feature only")
-      this.sendSocketNotification("INITIALIZED")
-      return
+      console.log("[DETECTOR] [DATA] Ready with Touch Screen Feature only");
+      this.sendSocketNotification("INITIALIZED");
+      return;
     }
 
-    this.config.mic.recorder= "arecord"
-    this.config.snowboyMicConfig.recorder= "arecord"
+    this.config.mic.recorder = "arecord";
+    this.config.snowboyMicConfig.recorder = "arecord";
 
     if (this.Porcupine.length) {
+
       /* Porcupine init */
-      this.porcupineConfig.accessKey = this.config.porcupineAccessKey || null
-      this.porcupineConfig.customModel = this.config.porcupineCustomModel ? __dirname + "/" + this.config.porcupineCustomModel : null
-      this.porcupineConfig.detectors = []
+      this.porcupineConfig.accessKey = this.config.porcupineAccessKey || null;
+      this.porcupineConfig.customModel = this.config.porcupineCustomModel ? `${__dirname}/${this.config.porcupineCustomModel}` : null;
+      this.porcupineConfig.detectors = [];
 
       if (!this.porcupineConfig.accessKey) {
-        this.sendSocketNotification("ACCESSKEY")
+        this.sendSocketNotification("ACCESSKEY");
       }
-      this.Porcupine.forEach(detector => {
-        const values = {}
+      this.Porcupine.forEach((detector) => {
+        const values = {};
         if (detector.Model) {
-          values.Model= detector.Model
-          values.Sensitivity= detector.Sensitivity ? detector.Sensitivity: 0.7
-          this.porcupineConfig.detectors.push(values)
+          values.Model = detector.Model;
+          values.Sensitivity = detector.Sensitivity ? detector.Sensitivity : 0.7;
+          this.porcupineConfig.detectors.push(values);
         }
-      })
-      log("[DATA] Porcupine DetectorConfig:", this.porcupineConfig)
-      this.porcupine = await new this.lib.Porcupine(this.porcupineConfig, this.config.mic, detect => this.onDetected("Porcupine", detect), this.config.debug)
-      this.porcupine.init()
+      });
+      log("[DATA] Porcupine DetectorConfig:", this.porcupineConfig);
+      this.porcupine = await new this.lib.Porcupine(this.porcupineConfig, this.config.mic, (detect) => this.onDetected("Porcupine", detect), this.config.debug);
+      this.porcupine.init();
       if (!this.porcupine.initialized) {
-        this.sendSocketNotification("PORCUPINENOTINIT")
-      } else {
-        if (this.porcupine.keywordNames && this.porcupine.keywordNames.length) {
-          console.log(`[DETECTOR] [DATA] Porcupine is initialized with ${this.porcupine.keywordNames.length} Models: ${this.porcupine.keywordNames.toString()}`)
-          this.detectorModel += this.porcupine.keywordNames.length
-        }
+        this.sendSocketNotification("PORCUPINENOTINIT");
+      } else if (this.porcupine.keywordNames && this.porcupine.keywordNames.length) {
+        console.log(`[DETECTOR] [DATA] Porcupine is initialized with ${this.porcupine.keywordNames.length} Models: ${this.porcupine.keywordNames.toString()}`);
+        this.detectorModel += this.porcupine.keywordNames.length;
       }
     }
 
     if (this.Snowboy.length) {
+
       /* Snowboy init */
-      this.snowboyConfig = this.Snowboy
-      log("[DATA] Snowboy DetectorConfig:", this.snowboyConfig)
-      this.snowboy = await new this.lib.Snowboy.Snowboy(this.snowboyConfig, this.config.snowboyMicConfig, detect => this.onDetected("Snowboy", detect), this.config.debug)
-      this.snowboy.init()
+      this.snowboyConfig = this.Snowboy;
+      log("[DATA] Snowboy DetectorConfig:", this.snowboyConfig);
+      this.snowboy = await new this.lib.Snowboy.Snowboy(this.snowboyConfig, this.config.snowboyMicConfig, (detect) => this.onDetected("Snowboy", detect), this.config.debug);
+      this.snowboy.init();
       if (this.snowboy.modelsNumber()) {
-        console.log(`[DETECTOR] [DATA] Snowboy is initialized with ${this.snowboy.modelsNumber()} Models: ${this.snowboy.modelsNames()}`)
-        this.detectorModel += this.snowboy.modelsNumber()
+        console.log(`[DETECTOR] [DATA] Snowboy is initialized with ${this.snowboy.modelsNumber()} Models: ${this.snowboy.modelsNames()}`);
+        this.detectorModel += this.snowboy.modelsNumber();
       }
     }
 
     if (this.detectorModel) {
-      this.sendSocketNotification("INITIALIZED")
-      console.log("[DETECTOR] [DATA] Initialized")
+      this.sendSocketNotification("INITIALIZED");
+      console.log("[DETECTOR] [DATA] Initialized");
     } else {
-      this.sendSocketNotification("NOT_INITIALIZED")
-      console.error("[DETECTOR] [DATA] No detector initialized!")
+      this.sendSocketNotification("NOT_INITIALIZED");
+      console.error("[DETECTOR] [DATA] No detector initialized!");
     }
   },
 
-  detectorFilter: function() {
-    return new Promise(resolve => {
-      this.Snowboy= this.config.detectors.filter(detector => detector.detector == "Snowboy")
-      this.Porcupine= this.config.detectors.filter(detector => detector.detector == "Porcupine")
-      resolve()
-    })
+  detectorFilter () {
+    return new Promise((resolve) => {
+      this.Snowboy = this.config.detectors.filter((detector) => detector.detector === "Snowboy");
+      this.Porcupine = this.config.detectors.filter((detector) => detector.detector === "Porcupine");
+      resolve();
+    });
   },
 
   /** [LIBRARY]  **/
-  libraries: function() {
-    let libraries= [
+  libraries () {
+    const libraries = [
       // { "library to load" : "store library name" }
       { "./components/porcupine": "Porcupine" },
       { "./components/snowboy": "Snowboy" }
-    ]
-    let errors = 0
-    return new Promise(resolve => {
-      libraries.forEach(library => {
+    ];
+    let errors = 0;
+    return new Promise((resolve) => {
+      libraries.forEach((library) => {
         for (const [name, configValues] of Object.entries(library)) {
-          let libraryToLoad = name
-          let libraryName = configValues
+          const libraryToLoad = name;
+          const libraryName = configValues;
 
           try {
             if (!this.lib[libraryName]) {
-              if ((libraryName == "Snowboy" && !this.Snowboy.length) || (libraryName == "Porcupine" && !this.Porcupine.length)) {
-                log("[LIBRARY] Ignored:", libraryToLoad)
+              if ((libraryName === "Snowboy" && !this.Snowboy.length) || (libraryName === "Porcupine" && !this.Porcupine.length)) {
+                log("[LIBRARY] Ignored:", libraryToLoad);
               } else {
-                this.lib[libraryName] = require(libraryToLoad)
-                log(`[LIBRARY] Loaded: ${libraryToLoad} --> this.lib.${libraryName}`)
+                this.lib[libraryName] = require(libraryToLoad);
+                log(`[LIBRARY] Loaded: ${libraryToLoad} --> this.lib.${libraryName}`);
               }
             }
-          } catch (e) {
-            console.error(`[DETECTOR] [LIBRARY] ${libraryToLoad} Loading error!` , e.message)
-            this.sendSocketNotification("WARNING" , {library: libraryToLoad })
-            errors++
-            this.lib.error = errors
+          } catch (err) {
+            console.error(`[DETECTOR] [LIBRARY] ${libraryToLoad} Loading error!`, err.message);
+            this.sendSocketNotification("WARNING", { library: libraryToLoad });
+            errors++;
+            this.lib.error = errors;
           }
         }
-      })
-      if (!errors) console.log("[DETECTOR] [LIBRARY] All libraries loaded!")
-      resolve(errors)
-    })
+      });
+      if (!errors) { console.log("[DETECTOR] [LIBRARY] All libraries loaded!"); }
+      resolve(errors);
+    });
   },
 
   /** [RULES] **/
-  activate: function() {
-    if (this.config.touchOnly) return this.sendSocketNotification("LISTENING")
+  activate () {
+    if (this.config.touchOnly) { return this.sendSocketNotification("LISTENING"); }
     if (this.porcupine && this.porcupine.initialized && (this.porcupine.keywordNames.length || this.porcupineCanRestart)) {
-      this.porcupine.start()
-      this.porcupineCanRestart = true
-      this.detector = true
+      this.porcupine.start();
+      this.porcupineCanRestart = true;
+      this.detector = true;
     }
     if (this.snowboy && this.snowboy.modelsNumber()) {
-      this.snowboy.start()
-      this.detector = true
+      this.snowboy.start();
+      this.detector = true;
     }
     if (this.detector) {
-      this.running = true
-      this.sendSocketNotification("LISTENING")
-      console.log(`[DETECTOR] [RULES] Start listening. ${this.detectorModel} Models`)
+      this.running = true;
+      this.sendSocketNotification("LISTENING");
+      console.log(`[DETECTOR] [RULES] Start listening. ${this.detectorModel} Models`);
+      return true;
     }
-    else {
-      this.sendSocketNotification("NOT_INITIALIZED")
-      console.error("[DETECTOR] [RULES] No detector initialized!")
-    }
+    this.sendSocketNotification("NOT_INITIALIZED");
+    console.error("[DETECTOR] [RULES] No detector initialized!");
+    return false;
   },
 
-  onDetected: function(from, detected) {
-    this.deactivate()
-    console.log(`[DETECTOR] [RULES] Detected: ${detected} from: ${from}`)
-    this.sendSocketNotification("DETECTED")
+  onDetected (from, detected) {
+    this.deactivate();
+    console.log(`[DETECTOR] [RULES] Detected: ${detected} from: ${from}`);
+    this.sendSocketNotification("DETECTED");
   },
 
-  deactivate: function(withNoti = true) {
-    if (this.config.touchOnly) return
+  deactivate (withNoti = true) {
+    if (this.config.touchOnly) { return; }
     if (this.porcupine) {
-      this.porcupine.stop()
-      this.detector = false
+      this.porcupine.stop();
+      this.detector = false;
     }
     if (this.snowboy) {
-      this.snowboy.stop()
-      this.detector= false
+      this.snowboy.stop();
+      this.detector = false;
     }
     if (!this.detector) {
-      this.running = false
-      if (withNoti) this.sendSocketNotification("DISABLED")
-      console.log("[DETECTOR] [RULES] Stop listening.")
+      this.running = false;
+      if (withNoti) { this.sendSocketNotification("DISABLED"); }
+      console.log("[DETECTOR] [RULES] Stop listening.");
     }
   }
-})
+});
